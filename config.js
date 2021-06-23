27e9008770c870b3356e3ba5dbcf0eec1b604df0
landmark2203 <86359559+landmark2203@users.noreply.github.com>
2021-06-23 20:02:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Violet màu xanh
     Có thể cậu đã biết
     Nhưng tớ cũng thương cậu`,
-    btnIntro: 'meow',
+    btnIntro: 'bấm vào đây nè',
     title: 'heaven is a place on earth with you ♡(˃͈ દ ˂͈ ༶ )',
     desc: 'liệu cậu có thể trở thành thiên đường trên mặt đất của tớ được không? ',
     btnYes: 'thương cậu mà',
-    btnNo: 'của riêng mình cậu thôi',
+    btnNo: 'để suy nghĩ đã',
     question:'ở bên tớ lâu thiệt lâu nhé?',
     btnReply: 'nhập cú pháp "người yêu ơi" để bắt đầu cuộc trò chuyện',
-    reply: 'ơi',
-    mess: 'tớ biết Thư là người yêu của tớ mà (っ˘з(˘⌣˘ ) ♡',
-    messDesc: 'người yêu của Mai Thảo.',
+    reply: 'người yêu ơi',
+    mess: 'tớ biết Thư biết nghe lời tớ mà (っ˘з(˘⌣˘ ) ♡',
+    messDesc: 'người yêu của Thư.',
     btnAccept: 'Ở bên nhau đến n tháng sau nhaaa',
     messLink: 'https://www.facebook.com/duongngocmai.thao/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
